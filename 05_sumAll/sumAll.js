2f0273c704387655b0ed538e1aecddb4113978a0
Esmond Yeo <esmond_yeo@>
2022-02-28 15:16:34 +0800

    Completed 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,39 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let sum = 0;
+    let smallSum = 0;
 
+    if (!((a || b) > 0) ){
+        return "ERROR";
+    } else if (typeof a !== "number" || typeof b !== "number") {
+        return "ERROR";
+    }
+    
+
+    if (a < b){
+        for (let i = 0; i < (b+1); i++){
+            sum += i;
+        }
+
+        for (let i = 0; i < a; i++){
+            smallSum += i;
+        }
+    } else {
+        for (let i = 0; i < (a+1); i++){
+            sum += i;
+        }
+
+        for (let i = 0; i < b; i++){
+            smallSum += i;
+        }
+    }
+
+
+    if (smallSum < sum){
+        return (sum - smallSum);
+    } else {
+        return (smallSum - sum);
+    }
+    
 };
 
 // Do not edit below this line
