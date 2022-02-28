dee958e4157629f1e8dd2ef211f29e823fbf3723
Esmond Yeo <esmond_yeo@>
2022-02-28 15:49:49 +0800

    Completed 06_leapYears and 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(x) {
+  let c = (x - 32) * 5 / 9;
+  c = Math.round(c * 10) / 10;
+  c = parseFloat(c.toFixed(1));
+  return c;
 };
 
-const ctof = function() {
-
+const ctof = function(y) {
+  let f = (y * 9 / 5) + 32;
+  f = Math.round(f * 10) / 10;
+  f = parseFloat(f.toFixed(1));
+  return f;
 };
 
 // Do not edit below this line
