5ae4e8b9dedfaa2e893182af2d33a494fc9a1925
Esmond Yeo <esmondyeoanimation@gmail.com>
2022-06-14 22:35:58 +0800

    completed 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (myString) {
+    let workingStr = myString.toLowerCase();
+    workingStr = workingStr.replace(/[^a-z]/g, "");
+    workingStr2 = workingStr.split("").reverse().join("");
+    return (workingStr == workingStr2);
 };
 
 // Do not edit below this line
