2a56f3de093876a0010f1e8fde21a5ed923f48a5
Esmond Yeo <esmondyeoanimation@gmail.com>
2022-06-14 22:12:57 +0800

    completed 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return (a+b);
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+	return (a - b);	
 };
 
-const sum = function() {
-	
+const sum = function (a) {
+	let b = 0;
+	for (let i = 0; i < a.length; i++) {
+		b += a[i];
+	}
+	return b;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+	let a = 0;
 
+	a = array[0];
+	for (let i = 0; i < (array.length-1); i++) {
+		a = a * array[i + 1];
+	}
+	return a;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+	if (a == 0) return 1;
+	let x = 1;
+	for (let i = a; i > 0; i--) {
+		x = x * i;
+    }
+	return x;
 };
 
 // Do not edit below this line
