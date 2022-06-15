d9832cd2a5e7a4569a9b7092e509c28be6e01d79
Esmond Yeo <esmond_yeo@>
2022-06-15 14:53:02 +0800

    completed 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,31 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let index = 0;
+    let born;
+    let die;
+    let ages = [];
+    for (let i = 0; i < arr.length; i++){
+        born = arr[i]["yearOfBirth"];
+        if ("yearOfDeath" in arr[i]){
+            die = arr[i]["yearOfDeath"];
+        }
+        else{
+            die = 2022;
+        }
+        ages.push(die-born);
+        }
+        console.log(ages);
+    
+    for (let i = 0; i < ages.length; i++){
+        let tempAgeIndex = ages.length-1;
+        if (ages[tempAgeIndex] > ages[i]){
+            index++;
+        }
+        else{
+            tempAgeIndex--;
+        }
+    }
+    
+    return arr[index];
 };
 
 // Do not edit below this line
